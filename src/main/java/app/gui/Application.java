ba7f3b525f933f026dc102188ed412d836488022
sHin7823 <migz.galang@mail.utoronto.ca>
2024-09-30 09:28:07 -0400

    Completed Task 3a

--- src/main/java/app/gui/Application.java
+++ src/main/java/app/gui/Application.java
@@ -247,6 +247,7 @@ public class Application {
         // make a separate line.
         final JButton getAverageButton = new JButton("Get Average Grade");
         // TODO Task 4: Add another button for "Get Top Grade" (check the getAverageButton for example)
+        final JButton getTopGrade = new JButton("Get Top Grade");
 
         final JButton leaveTeamButton = new JButton("Leave Team");
         final JLabel resultLabel = new JLabel();
@@ -265,6 +266,14 @@ public class Application {
         });
 
         // TODO Task 4: Add action listener for getTopGrade button, follow example of getAverageButton
+        getTopGrade.addActionListener(event -> {
+            try {
+                getAverageGradeUseCase.getAverageGrade(courseField.getText());
+            }
+            catch (Exception ex) {
+                JOptionPane.showMessageDialog(jFrame, ex.getMessage());
+            }
+        });
 
         leaveTeamButton.addActionListener(event -> {
             try {
